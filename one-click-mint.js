462187a793f46fa3792ee8a170af88c5fefa0c10
Pure-Peace <940857703@qq.com>
2021-12-10 16:28:53 +0800

    OK

--- one-click-mint.js
+++ one-click-mint.js
@@ -1,5 +1,83 @@
-const { readJsonToObject } = require('./utils/common')
+const { readJsonToObject, dateFormat } = require('./utils/common')
+const path = require('path')
+const fs = require('fs')
 
-const config = readJsonToObject(process.argv[2])
+/**
+ * @typedef {{
+ *   name: string,
+ *   cluster: string,
+ *   candyMachine: string,
+ *   mintUrl: string,
+ *   walletPrivKey: string,
+ *   mintCount: number,
+ *   logsDir: string
+ * }} OneClickMintConfigSchema
+ *
+ * @typedef {(msg) => void} Logger
+ **/
 
-console.log(config)
+/** @type {number} **/
+var RC = 0
+/** @type {OneClickMintConfigSchema} **/
+var config
+/** @type {string} **/
+var configPath
+/** @type {{ info: Logger, err: Logger, tx: Logger }} **/
+var log = {}
+/** @type {string || undefined} **/
+var LOGS_DIR
+/** @type {Date || undefined} **/
+var LOGS_DATE
+/** @type {string || undefined} **/
+var LOGS_DATE_TEXT
+
+function parseConfig() {
+  configPath = path.join(process.argv[2])
+  /** @type {OneClickMintConfigSchema} **/
+  config = readJsonToObject(configPath)
+  return { config, configPath }
+}
+
+/**
+ * @param {string} method
+ **/
+function registLogger(method) {
+  const file = fs.createWriteStream(path.join(LOGS_DIR, `${method}.out`), {
+    flags: 'w+',
+    encoding: 'utf-8',
+    autoClose: true,
+  })
+  log[method] = (msg) => {
+    RC++
+    file.write(typeof msg === 'string' ? msg : `${msg}`, 'utf-8', () => {
+      RC--
+    })
+  }
+}
+
+function initLoggers() {
+  LOGS_DATE = new Date()
+  LOGS_DATE_TEXT = dateFormat('YYYY-mm-dd_HH-MM-SS', LOGS_DATE)
+  LOGS_DIR = path.join(config.logsDir, `${config.name}_${LOGS_DATE_TEXT}`)
+  if (!fs.existsSync(LOGS_DIR)) {
+    fs.mkdirSync(LOGS_DIR)
+  }
+  ;['info', 'err', 'tx'].forEach((i) => {
+    registLogger(i)
+  })
+}
+
+async function main() {
+  parseConfig()
+  initLoggers()
+  log.tx(666)
+  log.err('asgasg')
+  log.info(444)
+}
+
+main().then(() => {
+  // Wait for all asynchronous operations to complete
+  setInterval(() => {
+    if (!RC) process.exit(0)
+  }, 100)
+})
--- utils/common.js
+++ utils/common.js
@@ -44,6 +44,32 @@ const readJsonToObject = (path) => {
 }
 
 /**
+ * @param {string} fmt
+ * @param {Date} date
+ **/
+function dateFormat(fmt, date) {
+  let ret
+  let opt = {
+    'Y+': date.getFullYear().toString(),
+    'm+': (date.getMonth() + 1).toString(),
+    'd+': date.getDate().toString(),
+    'H+': date.getHours().toString(),
+    'M+': date.getMinutes().toString(),
+    'S+': date.getSeconds().toString(),
+  }
+  for (let k in opt) {
+    ret = new RegExp('(' + k + ')').exec(fmt)
+    if (ret) {
+      fmt = fmt.replace(
+        ret[1],
+        ret[1].length == 1 ? opt[k] : opt[k].padStart(ret[1].length, '0'),
+      )
+    }
+  }
+  return fmt
+}
+
+/**
  * @param {number} price
  * @param {number} mantissa
  **/
@@ -51,11 +77,11 @@ const parsePrice = (price, mantissa) => {
   return Math.ceil(parseFloat(price) * mantissa)
 }
 
-
 module.exports = {
   listDir,
   rmDir,
   urlToDir,
   readJsonToObject,
-  parsePrice
+  parsePrice,
+  dateFormat,
 }
